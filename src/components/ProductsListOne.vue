367e6d107051dad549b9571902d52e14b79d6d1b
jodieblue <myjodieblue@gmail.com>
2021-03-05 11:57:02 +0800

    use getters

--- src/components/ProductsListOne.vue
+++ src/components/ProductsListOne.vue
@@ -1,7 +1,7 @@
 <template>
   <h1 class="mt-4 text-2xl">list One</h1>
   <ul class="w-full flex flex-row justify-evenly item-center bg-indigo-200 p-4">
-    <li v-for="product in products" class="flex flex-row space-x-2 p-4 bg-gray-100 rounded">
+    <li v-for="product in saleProducts" class="flex flex-row space-x-2 p-4 bg-gray-100 rounded">
       <span>{{product.name}}</span>
       <span>${{product.price}}</span>
     </li>
@@ -14,6 +14,9 @@ export default {
  computed:{
    products(){
      return this.$store.state.products;
+   },
+   saleProducts(){
+     return this.$store.getters.saleProducts;
    }
  }
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -7,5 +7,16 @@ export default createStore({
       { name: 'Orange', price: '20' },
       { name: 'pearl', price: '24' }
     ]
+  },
+  getters: {
+    saleProducts: state => {
+      var saleProducts = state.products.map((product: { name: string; price: number; }) => {
+        return {
+          name: '**' + product.name + '**',
+          price: product.price / 2
+        };
+      });
+      return saleProducts;
+    }
   }
 })
